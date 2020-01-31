a4f3a5a944622c9bbf5269373f104c65e62f0537
danielq <qiaoer0505@gmail.com>
2020-02-01 10:38:16 +1100

    move seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+m1 = Menu.create!(name: "Contacts", sort: 1)
+
+in_person = m1.submenus.create!(name: "In-Person", sort: 1)
+MenuItem.create!(name: "Our-shops", sort: 1, menu: m1, submenu: in_person)
+MenuItem.create!(name: "Partners", sort: 2, menu: m1, submenu: in_person)
+
+online = m1.submenus.create!(name: "Online", sort: 2)
+MenuItem.create!(name: "Email", sort: 1, menu: m1, submenu: online)
+MenuItem.create!(name: "Online-Chat", sort: 2, menu: m1, submenu: online)
+
+m2 = Menu.create!(name: "FAQs", sort: 2)
+
+cq = m2.submenus.create!(name: "Common-Questions", sort: 1)
+MenuItem.create(name: "How-to-login", sort: 1, menu: m2, submenu: cq)
+MenuItem.create(name: "How-to-register", sort: 2, menu: m2, submenu: cq)
+
+more = m2.submenus.create(name: "More", sort: 2)
+MenuItem.create(name: "More-Questions", sort: 1, menu: m2, submenu: more)
+
+puts "seeded #{Menu.count} Menus, #{Submenu.count} Submenus and #{MenuItem.count} Menuitems"
